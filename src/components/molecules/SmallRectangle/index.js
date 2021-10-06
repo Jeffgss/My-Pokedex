588f2a135c4607c0ae82feaf4d52e7c1f1605ea5
Jeff <jefferson.santos@maxmilhas.com.br>
2021-10-06 08:45:14 -0300

    create small rectangle component

---
+++ src/components/molecules/SmallRectangle/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const SmallRectangle = ({ ...props }) => <Container {...props} />;
+
+export default SmallRectangle;
---
+++ src/components/molecules/SmallRectangle/styles.js
@@ -0,0 +1,33 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+  width: 40px;
+  height: 10px;
+  border-radius: 10px;
+  border: 2px solid #492436;
+  box-shadow: inset 1px 2px rgb(255 255 255 / 60%);
+
+  ${(props) =>
+    props.color === "red" &&
+    css`
+      background: var(--redLand);
+    `}
+
+  ${(props) =>
+    props.color === "yellow" &&
+    css`
+      background: var(--yellow);
+    `}
+
+  ${(props) =>
+    props.color === "blue" &&
+    css`
+      background: var(--blue);
+    `}
+
+    ${(props) =>
+    props.color === "darkBlue" &&
+    css`
+      background: var(--darkBlue);
+    `}
+`;
