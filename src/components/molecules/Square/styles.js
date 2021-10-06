0f173c3385b005afc447464dd18a84f9fb497cb7
Jeff <jefferson.santos@maxmilhas.com.br>
2021-10-06 08:50:56 -0300

    create square component

---
+++ src/components/molecules/Square/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 41px;
+  height: 41px;
+  background: #5493b2;
+  border-radius: 12px;
+  border: 2px solid #492436;
+  box-shadow: inset 3px 3px rgb(255 255 255 / 40%),
+    inset -3px -1px rgb(0 0 0 / 30%);
+`;
