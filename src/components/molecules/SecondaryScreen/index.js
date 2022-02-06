a989018b44125d6f77ab96f2a1eb49808473d949
Jeff <jefferson.santos@maxmilhas.com.br>
2022-02-06 20:25:56 -0300

    display details on secondary screen

--- src/components/molecules/SecondaryScreen/index.js
+++ src/components/molecules/SecondaryScreen/index.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { Container } from "./styles";
 
 import ScreenEdge from "../../atoms/ScreenEdge";
 import Display from "../../atoms/Display";
+import { PokemonContext } from "../../../PokeContext";
 
-const SecondaryScreen = () => (
-  <Container>
-    <ScreenEdge type="secondary">
-      <Display />
-    </ScreenEdge>
-  </Container>
-);
+const SecondaryScreen = () => {
+  const pokemon = useContext(PokemonContext);
+  const pokemonTypes = pokemon.catchSearchedPokemon.types;
+  const pokemonHp = pokemon.catchSearchedPokemon.hp;
+  const pokemonAttack = pokemon.catchSearchedPokemon.attack;
+  const pokemonDefense = pokemon.catchSearchedPokemon.defense;
+
+  console.log(!!pokemon);
+
+  return (
+    <Container>
+      <ScreenEdge type="secondary">
+        <Display>
+          {pokemonTypes && (
+            <>
+              <span>
+                Hp: <strong>{pokemonHp}</strong>
+              </span>
+              <span>
+                Attack: <strong>{pokemonAttack}</strong>
+              </span>
+              <span>
+                Defense: <strong>{pokemonDefense}</strong>
+              </span>
+              <span>
+                Type: <strong>{pokemonTypes}</strong>
+              </span>
+            </>
+          )}
+        </Display>
+      </ScreenEdge>
+    </Container>
+  );
+};
 
 export default SecondaryScreen;
