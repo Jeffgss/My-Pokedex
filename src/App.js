911002cdb289d4f119af162dd22896e7f5b0a9e5
Jeff <jefferson.santos@maxmilhas.com.br>
2022-02-06 20:29:24 -0300

    assemble pokedex

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 
-import LeftSide from "./pages/LeftSide";
 import { PokemonProvider } from "./PokeContext";
+
+import LeftSide from "./pages/LeftSide";
+import RightSide from "./pages/RightSide";
 
 import "./App.css";
 import GlobalStyle from "./styles/global";
 
 const App = () => (
-  <PokemonProvider>
-    <LeftSide />
-    <GlobalStyle />
-  </PokemonProvider>
+  <div className="App">
+    <PokemonProvider>
+      <LeftSide />
+      <RightSide />
+      <GlobalStyle />
+    </PokemonProvider>
+  </div>
 );
 
 export default App;
